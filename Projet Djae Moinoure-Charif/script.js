15132db68b1901a00d7db88bf5a7ec469c6a3217
m4djae <moinourecharifdjae@gmail.com>
2025-02-02 21:08:48 +0100

    Add files via upload

---
+++ Projet Djae Moinoure-Charif/script.js
@@ -0,0 +1,7 @@
+
+   const button = document.getElementById('changeColorButton');
+
+        // Ajouter un événement 'click' pour changer la couleur de la page
+        button.addEventListener('click', function() {
+            document.body.style.backgroundColor = 'darkblue'; // Changer la couleur de fond en bleu clair
+        });
